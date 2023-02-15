d05ceb7b0c8b75d5492dc5ccf81448caf7df74f8
AnatoliiBelov <anatoly.belov.ne@gmail.om>
2023-02-15 12:27:19 +0300

    сделал, что мог

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+        printNumberWordsInText("мама мыла раму. раму мыла мама");
+        System.out.println(printStatistics("мама мыла раму. раму мыла мама"));
     }
+
+
+
+
+    public static void printNumberWordsInText(String text) {
+        System.out.println("В тексте " + text.split(" ").length + " слов/а");
+
+    }
+
+    public static HashMap<String, Integer> printStatistics(String text) {
+        HashMap<String, Integer> counterWord = new HashMap<>();
+
+        for (String s : text.split(" ")
+        ) {
+            if (!counterWord.containsKey(s)) {
+                counterWord.put(s, 1);
+            } else {
+                int count = counterWord.get(s);
+                count++;
+                counterWord.put(s, count);
+            }
+
+        }
+        return counterWord;
+    }
+
+
+
+
 }
---
+++ src/StatisticsOfText.java
@@ -0,0 +1,9 @@
+public class StatisticsOfText {
+    private String word;
+    private int counter;
+
+    public StatisticsOfText(String word, int counter) {
+        this.word = word;
+        this.counter = counter;
+    }
+}
